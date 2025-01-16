39d4b2b0375f79572cceca54f0dee0b21df62f83
Bar9cc5 <fanimom5@gmail.com>
2025-01-16 23:30:13 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.querySelectorAll('.menu-item').forEach(item => {
+    item.addEventListener('mouseenter', () => {
+        item.querySelector('.submenu').style.display = 'block';
+    });
+
+    item.addEventListener('mouseleave', () => {
+        item.querySelector('.submenu').style.display = 'none';
+    });
+});
